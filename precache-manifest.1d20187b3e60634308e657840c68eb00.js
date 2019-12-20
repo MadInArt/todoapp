29d4acc98cb966d24ab0c2424568711ba5660e61
MadInArt <knyazvladimir26@gmail.com>
2019-12-20 10:04:58 +0100

    Updates

---
+++ precache-manifest.1d20187b3e60634308e657840c68eb00.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "010e67cab851071eada659868d344827",
+    "url": "/todoapp/index.html"
+  },
+  {
+    "revision": "67bb47dfa8c9ec6b79ab",
+    "url": "/todoapp/static/css/main.4a98772f.chunk.css"
+  },
+  {
+    "revision": "ddd59bd58fc4d2bf818b",
+    "url": "/todoapp/static/js/2.f5b89567.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/todoapp/static/js/2.f5b89567.chunk.js.LICENSE"
+  },
+  {
+    "revision": "67bb47dfa8c9ec6b79ab",
+    "url": "/todoapp/static/js/main.5c713c48.chunk.js"
+  },
+  {
+    "revision": "77ba27e0b4b297640a85",
+    "url": "/todoapp/static/js/runtime-main.b2673753.js"
+  }
+]);
